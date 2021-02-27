1e987cd684ad626c221c7dc91a6a393d8f2e5cb9
patto <michaelpatterson@autotechfm.com>
2021-02-27 16:31:04 +1100

    iOS bug fixes from testing

--- ios/Plugin/FileSharerPlugin.swift
+++ ios/Plugin/FileSharerPlugin.swift
@@ -19,15 +19,15 @@ public class FileSharerPlugin: CAPPlugin {
     @objc func share(_ call: CAPPluginCall) {
         guard let filename = call.getString("filename") else {
            call.reject("Filename parameter not provided")
+            return
         }
         guard let base64Data = call.getString("base64Data") else {
            call.reject("Base64Data parameter not provided")
-        }
-        guard let contentType = call.getString("contentType") else {
-           call.reject("ContentType parameter not provided")
+            return
         }
         guard let fileData = Data(base64Encoded: base64Data) else {
             call.reject("The base64 data provided is invalid")
+            return
         }
 
         let tempFilePathUrl = FileManager.default.temporaryDirectory.appendingPathComponent(filename)
@@ -38,12 +38,12 @@ public class FileSharerPlugin: CAPPlugin {
             DispatchQueue.main.async {
                 let deviceType = UIDevice.current.model
                 if(deviceType.range(of: "iPad") != nil) {
-                    self.setCenteredPopover(ourCustomViewController)
-                        self.bridge.present(viewCtrl, animated: true, completion: {
+                    self.setCenteredPopover(viewCtrl)
+                    self.bridge?.viewController?.present(viewCtrl, animated: true, completion: {
                             call.resolve()
                         })
-                } else if(deviceType.range(of: "iPhone")) {
-                    self.bridge.present(viewCtrl, animated: true, completion: {
+                } else if(deviceType.range(of: "iPhone") != nil) {
+                    self.bridge?.viewController?.present(viewCtrl, animated: true, completion: {
                             call.resolve()
                         })
                 } else {
